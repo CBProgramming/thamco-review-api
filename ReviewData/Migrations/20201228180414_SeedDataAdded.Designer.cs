43620f6c254405c61b7944d2286a919ff35856a4
ChrisBurrellT7145969 <t7145969@live.tees.ac.uk>
2020-12-28 18:04:28 +0000

    Seed data added to db

--- ReviewData/Migrations/20201228180414_SeedDataAdded.Designer.cs
+++ ReviewData/Migrations/20201228180414_SeedDataAdded.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReviewData;
 
 namespace ReviewData.Migrations
 {
     [DbContext(typeof(ReviewDb))]
-    partial class ReviewDbModelSnapshot : ModelSnapshot
+    [Migration("20201228180414_SeedDataAdded")]
+    partial class SeedDataAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
