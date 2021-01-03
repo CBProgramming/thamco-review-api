9dd488f5d62ee721f8c73447eb16cb3550095e3e
ChrisBurrellT7145969 <t7145969@live.tees.ac.uk>
2021-01-03 13:34:02 +0000

    Auth Ids Updated

--- ReviewData/ReviewDb.cs
+++ ReviewData/ReviewDb.cs
@@ -48,31 +48,31 @@ namespace ReviewData
                 new Customer
                 {
                     CustomerId = 1,
-                    CustomerAuthId = "b45727f0-bf10-40dc-a687-f5cd025630f2",
+                    CustomerAuthId = "a64c9beb-534a-4b40-a9be-58ed21597cd0",
                     CustomerName = "Chris Burrell"
                 },
                 new Customer
                 {
                     CustomerId = 2,
-                    CustomerAuthId = "286fa26e-ae5f-4c5a-b89d-7301fb247d78",
+                    CustomerAuthId = "8e689e3c-24b1-400c-a8ad-7435c4fd15b5",
                     CustomerName = "Paul Mitchell"
                 },
                 new Customer
                 {
                     CustomerId = 3,
-                    CustomerAuthId = "b477a6e4-6607-43c9-8ea0-c2367a5b0360",
+                    CustomerAuthId = "94d6c9b0-b3c8-4ad6-96ed-c7ab43d6dd23",
                     CustomerName = "Jack Ferguson"
                 },
                 new Customer
                 {
                     CustomerId = 4,
-                    CustomerAuthId = "9fe723cf-7ac2-4b51-a79a-9e5813fb306a",
+                    CustomerAuthId = "0313a3ca-e9d0-43c3-a580-ab25c6b224d8",
                     CustomerName = "Carter Ridgeway"
                 },
                 new Customer
                 {
                     CustomerId = 5,
-                    CustomerAuthId = "727c783f-ede7-4e53-a365-f8c830e327f4",
+                    CustomerAuthId = "8de93d90-7e62-40e9-8032-602f835ee8ee",
                     CustomerName = "Karl Hall"
                 });
 
